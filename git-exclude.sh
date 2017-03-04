231394403cdccf8367be4d5f82b5cb04d76f67b3
Alex Day <ahday4@gmail.com>
2017-03-04 13:26:36 -0500

    Initial commit

---
+++ git-exclude.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Declare variables
+git_exclude="./.git/info/exclude"
+
+#Declare functions
+check_exclude() {
+	if [ -e $git_exclude ]; then
+		echo "File found"
+	else
+		echo "File not found"
+		exit 1
+	fi
+}
+
+read_input() {
+	echo "Please enter file(s) to be excluded"
+	read files
+	for file in $files; do
+		echo ">${file} excluded"
+		echo $file >> $git_exclude
+	done
+	exit 0
+}
+
+echo "This script appends to your git repository's exclude file"
+
+#TODO check for presence of ./.git/info/exclude else return error
+check_exclude
+
+#TODO for each argument at invocation append to ./.git/info/exclude
+read_input
